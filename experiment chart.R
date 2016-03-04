7234b8b395e6af832a69546d87de91b5730b1139
edwardlxb <lux23@uw.edu>
2016-03-03 16:39:05 -0800

    with data

---
+++ experiment chart.R
@@ -0,0 +1,8 @@
+library(xlsx)
+library(jsonlite)
+#setwd("/Users/Edward/Documents/Final-Project-X/data")
+#data<- read.xlsx(file = "gands.xls", sheetIndex = "BOP History")
+#View(data)
+#max_max()
+
+base<- "https://www.data.gov/app/uploads/2016/02/opendatasites.csv"
