2786f1d1034031e360fe005f9c289b29d6b091dd
edwardlxb <lux23@uw.edu>
2016-03-09 00:41:51 -0800

    map_update

--- scripts/build_map.R
+++ scripts/build_map.R
@@ -1,7 +1,7 @@
 library(dplyr)
 library(plotly)
 
-setwd("/Users/Edward/Documents/Final-Project-X/data")
+setwd("/Users/Edward/Documents/Final-Project-X/shiny-app/data")
 
 
 #data<- read.csv("Most+Recent+Cohorts+(All+Data+Elements).csv")
@@ -23,20 +23,33 @@ newdata<- read.csv("Summarized data.csv")
 add = newdata
 
   
-newdata$hover <- paste(newdata$INSTNM, "has", newdata$ADM_RATE, "percent of admission rate", 
+newdata$hover <- paste(newdata$INSTNM, "has", newdata$ADM_RATE * 100, "percent of admission rate", 
                         '<br>', "Location:",newdata$CITY, newdata$ZIP)  
   
 cols <- RColorBrewer::brewer.pal(3, "Set1")
 
 newdata$ADM_RATE <- as.numeric(as.character(newdata$ADM_RATE))
 
-build_map<- function(add, states){
+newdata$SAT_AVG<- as.numeric(as.character(newdata$SAT_AVG))
+
+
+
+
+
+build_map<- function(add, lower_SAT, upper_SAT){
   
+#state_fun<- add%>% 
+  #          filter(STABBR == states)
+
+final_data<- add %>%
+            filter( upper_SAT >= SAT_AVG) %>%
+            filter( lower_SAT <= SAT_AVG) %>%
+            select(INSTNM, LATITUDE, LONGITUDE, ADM_RATE, hover)
+
   m <- list(
     colorbar = list(title = "Admission Rate"),
-    size = 5, opacity = 0.8, symbol = 'circle')
+    size = 5, opacity = 0.5, symbol = 'circle')
 
-                      
   g <- list(
     scope = 'usa',
     projection = list(type = 'albers usa'),
@@ -48,18 +61,17 @@ build_map<- function(add, states){
     subunitwidth = 0.5)
   
   plot_ly(
-    add%>% 
-      filter(STABBR == states)
-      
-      , lat = LATITUDE, lon = LONGITUDE, 
+  final_data, 
+    lat = LATITUDE,
+    lon = LONGITUDE, 
           text = hover, 
           colors = cols, 
-          color = ADM_RATE, 
+          color = ADM_RATE * 100, 
           opacity = 0.5, na.rm = TRUE, 
           type = 'scattergeo', 
           locationmode = 'USA-states', 
+          size = ADM_RATE, 
           mode = "markers", 
-
           marker = m) %>%
     layout(title = "US College Cards", geo = g)
 }
